a255271b40a59d09729f1c90e0ece3dade6c63fb
adatu <adatu@signet.tv>
2019-12-22 23:37:01 -0800

    adding recorded screen

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # meeting-noter
 small python script to convert audio to text with timestamp
+
+## Requirements
+1. Python 3 or 2
+2. Pyaudio
+3. speech_recognition (install via pip)
+
+## Notes
+Execute the script using: `python3 speech_recog.py`. 
+Once done, use "stop recording" to stop the script. 
+The converted text is saved in converted_audio.txt
+ 
+ 
---
+++ speech_recog.py
@@ -0,0 +1,25 @@
+import time
+import speech_recognition as sr
+from datetime import datetime
+
+r = sr.Recognizer()
+fs = open("converted_audio.text","w+")
+
+with sr.Microphone() as source:
+	print("Audio conversion now:")
+	while True:
+		audio = r.listen(source)
+		try:
+			text = r.recognize_google(audio)
+			n = datetime.now()			
+			fs.write('{} -- {}\n'.format(n.strftime("%Y-%m-%d %H:%M:%S"),text))
+			print("recorded: {}".format(text))
+			time.sleep(.5)
+			if text == "stop recording":
+				print("Ending recording session")
+				fs.close()
+				break
+		except sr.UnknownValueError:
+			print("Google speech recognition could not understand your audio")
+		except sr.RequestError as e:
+			print("Could not request results from Google")
